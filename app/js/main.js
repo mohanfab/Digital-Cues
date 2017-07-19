d0c5ab53a11aec1de013b38fe6260c9751716d38
mohanfab <pardeshi.moha@gmail.com>
2017-07-19 13:55:20 +0530

    my work desc

--- app/js/main.js
+++ app/js/main.js
@@ -80,6 +80,7 @@ mainApp.controller('addemployee', ['$scope', '$rootScope', '$http', function($sc
         $scope.searchIndex = id;
         $scope.userSearch = angular.copy($scope.userList[id]);
     }
+   
     $scope.companyedit = function(id) {
         $scope.addcompanyname = true;
         $scope.addecompanyistflag = false;
@@ -102,3 +103,22 @@ mainApp.controller('addemployee', ['$scope', '$rootScope', '$http', function($sc
     }
 
 }]);
+mainApp.directive('formatPhone', [
+        function() {
+            return {
+                require: 'ngModel',
+                restrict: 'A',
+                link: function(scope, elem, attrs, ctrl, ngModel) {
+                    elem.add(phonenumber).on('keyup', function() {
+                       var origVal = elem.val().replace(/[^\w\s]/gi, '');
+                       if(origVal.length === 10) {
+                         var str = origVal.replace(/(.{3})/g,"$1-");
+                         var phone = str.slice(0, -2) + str.slice(-1);
+                         jQuery("#phonenumber").val(phone);
+                       }
+
+                    });
+                }
+            };
+        }
+    ]);
